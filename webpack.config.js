e67109b1d61f1e37c8dbf433917b9ab05b920cd0
Dmitrij <lukdmitrij@yandex.ru>
2023-03-08 00:09:14 +0300

    create template.pug

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'template.html'),
+            // template: path.join(__dirname, 'src', 'template.html'),
+            template: path.join(__dirname, 'src', 'template.pug'),
             filename: 'index.html',
         }),
         new FileManagerPlugin({
